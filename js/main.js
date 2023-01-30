cb9f6862b41e85e9214784546d63ae7140e60fbc
Rymma Savchuk <savchukrmm@gmail.com>
2023-01-30 22:02:41 +0200

    Responsive layout was created

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
+// Carousel
 const owl = $('.owl-carousel');
 
 owl.owlCarousel({
   center: true,
   loop: true,
-  margin: 30,
-  startPosition: 1,
-  items: 3,
+  margin: 20,
+  startPosition: 0,
+  items: 1,
+  responsive: {
+    540: {
+      items: 3,
+      startPosition: 1,
+    },
+    1200: {
+      items: 3,
+      margin: 30,
+    },
+  },
 });
 
 $('.slider__btn--prev').click(function () {
@@ -15,3 +26,14 @@ $('.slider__btn--prev').click(function () {
 $('.slider__btn--next').click(function () {
   owl.trigger('next.owl.carousel');
 });
+
+// Nav icon
+const navBtn = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav');
+const menuIcon = document.querySelector('.menu-icon');
+
+navBtn.onclick = function () {
+  nav.classList.toggle('nav--mobile');
+  menuIcon.classList.toggle('menu-icon--active');
+  document.body.classList.toggle('no-scroll');
+};
